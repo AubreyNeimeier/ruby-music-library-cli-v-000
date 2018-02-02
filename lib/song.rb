64f667a52b4df23065479b79e0a53ee463bbb5b2
Aubrey Neimeier <neimeier.1@osu.edu>
2018-02-02 18:13:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -64,7 +64,7 @@ class Song
     #condensed could be written
 =end
     parts = file.split(" - ")
-    artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
+    artist_string, song, genre_string = parts[0], parts[1], parts[2].gsub(".mp3", "")
 
     artist = Artist.find_or_create_by_name(artist_string)
     genre = Genre.find_or_create_by_name(genre_string)
